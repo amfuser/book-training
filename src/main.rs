f6899885c785a3f5f7342d9f2f93158898dbb3e4
amfuser <amf137@yahoo.com>
2023-09-15 18:24:38 -0600

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    println!("Hello, world!");
+    println!("{}", gcd(400, 325));
+}
+
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+///
+/// This will be a function of my own writing that performs gcd
+///
+// fn myGcd() {
+//
+//}
+
+//
+// Tests
+//
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
+                   3 * 7 * 11 * 13 * 19),
+               3 * 11);
+}
